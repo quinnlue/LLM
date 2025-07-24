c3cbf03fdeef8715c6dcbe69a34a06775f5321f2
Quinn Lue <luequinn@gmail.com>
2025-07-24 11:21:18 -0600

    add tokenizer loop file

--- src/preprocess/add_tokens.py
+++ src/preprocess/add_tokens.py
@@ -1,9 +1,6 @@
 import sys
 import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-
-import tqdm
+from tqdm import tqdm
 import pandas as pd
 from src.tokenizer.tokenizer import Tokenizer
 
@@ -14,22 +11,25 @@ def tokenize(text, tokenizer):
 
 
 if __name__ == "__main__":
-    tqdm.pandas()
-
     train = pd.read_parquet("../../data/clean/train.parquet")
     test = pd.read_parquet("../../data/clean/test.parquet")
     validation = pd.read_parquet("../../data/clean/validation.parquet")
     tokenizer = Tokenizer(token_to_id_path=r"..\tokenizer\token_to_id.json", merges_path=r"..\tokenizer\merges.json")
 
-    validation['text'] = validation['text'].progress_apply(lambda x: tokenize(x, tokenizer))
+    tokenized_validation = []
+    for x in tqdm(validation['text'], desc="Tokenizing validation"):
+        tokenized_validation.append(tokenize(x, tokenizer))
+    validation['text'] = tokenized_validation
     validation.to_parquet("../../data/tokenized/validation.parquet")
 
-    test['text'] = test['text'].progress_apply(lambda x: tokenize(x, tokenizer))
+    tokenized_test = []
+    for x in tqdm(test['text'], desc="Tokenizing test"):
+        tokenized_test.append(tokenize(x, tokenizer))
+    test['text'] = tokenized_test
     test.to_parquet("../../data/tokenized/test.parquet")
 
-    train['text'] = train['text'].progress_apply(lambda x: tokenize(x, tokenizer))
+    tokenized_train = []
+    for x in tqdm(train['text'], desc="Tokenizing train"):
+        tokenized_train.append(tokenize(x, tokenizer))
+    train['text'] = tokenized_train
     train.to_parquet("../../data/tokenized/train.parquet")
-
-
-
-
