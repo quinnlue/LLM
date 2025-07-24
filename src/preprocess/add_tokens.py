897caa13539e492aaf33f45e463de43291f9d33d
Quinn Lue <luequinn@gmail.com>
2025-07-24 11:22:23 -0600

    add tokenizer loop file

--- src/preprocess/add_tokens.py
+++ src/preprocess/add_tokens.py
@@ -1,5 +1,8 @@
 import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
 from tqdm import tqdm
 import pandas as pd
 from src.tokenizer.tokenizer import Tokenizer
