2b0328eb18b3d57cd08ed19767ddc634351b84c5
Quinn Lue <luequinn@gmail.com>
2025-07-24 16:12:13 -0600

    add token test

--- src/preprocess/add_tokens.py
+++ src/preprocess/add_tokens.py
@@ -1,23 +1,46 @@
 import sys
 import os
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 from tqdm import tqdm
 import pandas as pd
 from src.tokenizer.tokenizer import Tokenizer
-import swifter
-tqdm.pandas()
 import time
+from multiprocessing import Pool
 
-# Get the project root directory
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-def tokenize(text, tokenizer):
-    seq = tokenizer.encode(text)
-    return seq
+# Global tokenizer variable for workers
+tokenizer = None
+
+def init_worker():
+    global tokenizer
+    tokenizer = Tokenizer(
+        token_to_id_path=os.path.join(PROJECT_ROOT, "src/tokenizer/token_to_id.json"), 
+        merges_path=os.path.join(PROJECT_ROOT, "src/tokenizer/merges.json")
+    )
+
+def tokenize(text):
+    global tokenizer
+    return tokenizer.encode(text)
 
 if __name__ == "__main__":
+    validation = pd.read_parquet(os.path.join(PROJECT_ROOT, "data/clean/validation.parquet"))
+
+    start_time = time.time()
+
+    texts = validation['text'].loc[:99].tolist()
+    processes = os.cpu_count()
+    with Pool(processes=os.cpu_count(), initializer=init_worker) as pool:
+        out = list(tqdm(pool.imap(tokenize, texts), total=len(texts)))
+
+    end_time = time.time()
+
+    total_tokens = sum(len(x) for x in out)
+    print(f"Time taken: {end_time - start_time} seconds")
+    print(f"Total tokens: {total_tokens}")
+    print(f"Tokens per second: {total_tokens / (end_time - start_time)}")
+
     # text = "Your input text here"
     # tokenizer = Tokenizer()
     # print(tokenizer.encode(text))
@@ -25,20 +48,7 @@ if __name__ == "__main__":
     # Use absolute paths based on project root
     # train = pd.read_parquet(os.path.join(PROJECT_ROOT, "data/clean/train.parquet"))
     # test = pd.read_parquet(os.path.join(PROJECT_ROOT, "data/clean/test.parquet"))
-    validation = pd.read_parquet(os.path.join(PROJECT_ROOT, "data/clean/validation.parquet"))
-    tokenizer = Tokenizer(
-        token_to_id_path=os.path.join(PROJECT_ROOT, "src/tokenizer/token_to_id.json"), 
-        merges_path=os.path.join(PROJECT_ROOT, "src/tokenizer/merges.json")
-    )
 
-    start_time = time.time()
-    validation.loc[:99, 'seq'] = validation.loc[:99, 'text'].apply(lambda x: tokenize(x, tokenizer))
-    end_time = time.time()
-
-    total_tokens = validation['seq'].dropna().apply(len).sum()
-    print(f"Time taken: {end_time - start_time} seconds")
-    print(f"Total tokens: {total_tokens}")
-    print(f"Tokens per second: {total_tokens / (end_time - start_time)}")
 
     # tokenized_test = []
     # for x in tqdm(test['text'], desc="Tokenizing test"):
