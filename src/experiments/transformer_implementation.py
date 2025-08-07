2538b849ece91cda1451bf1bbb937bf013850f10
Quinn Lue <luequinn@gmail.com>
2025-08-06 18:32:31 -0600

    removed safety

--- src/experiments/transformer_implementation.py
+++ src/experiments/transformer_implementation.py
@@ -13,70 +13,69 @@ from src.tokenizer.tokenizer import Tokenizer
 import pandas as pd
 
 
-class Test(Module):
+src = np.random.randint(low=1, high=16, size=(128, 16))
+x = src[:, :-1]
+y = src[:, 1:]
+
+x_mine = Tensor(x, requires_grad=False)
+y_mine = Tensor(y, requires_grad=False)
+
+
+class Net(Module):
     def __init__(self, d_model, n_heads, vocab_size, max_seq_len, pad_idx=0):
         super().__init__()
-        self.e = self.embedding(vocab_size, d_model, max_seq_len, pad_idx)
+        self.e = self.embedding(vocab_size, d_model, max_seq_len, pad_idx, name="Embedding")
 
         self.head1 = self.transformer(d_model=d_model, n_heads=n_heads)
         self.head2 = self.transformer(d_model=d_model, n_heads=n_heads)
         self.head3 = self.transformer(d_model=d_model, n_heads=n_heads)
-        self.head4 = self.transformer(d_model=d_model, n_heads=n_heads)
-        self.head5 = self.transformer(d_model=d_model, n_heads=n_heads)
-        self.head6 = self.transformer(d_model=d_model, n_heads=n_heads)
-        self.head7 = self.transformer(d_model=d_model, n_heads=n_heads)
-        self.head8 = self.transformer(d_model=d_model, n_heads=n_heads)
-        self.project = self.linear(d_model, vocab_size, module_type="linear", layer_type="linear", name="project")
+        # self.head4 = self.transformer(d_model=d_model, n_heads=n_heads)
+        # self.head5 = self.transformer(d_model=d_model, n_heads=n_heads)
+        # self.head6 = self.transformer(d_model=d_model, n_heads=n_heads)
+        # self.head7 = self.transformer(d_model=d_model, n_heads=n_heads)
+        # self.head8 = self.transformer(d_model=d_model, n_heads=n_heads)
+        self.project = self.linear(d_model, vocab_size, name="project")
     
     def forward(self, idx):
         x, padding_mask = self.e.get_sentence_embedding(idx)
+        x = Tensor(x.data, requires_grad=False)
         x = self.head1(x, padding_mask)
         x = self.head2(x, padding_mask)
         x = self.head3(x, padding_mask)
-        x = self.head4(x, padding_mask)
-        x = self.head5(x, padding_mask)
-        x = self.head6(x, padding_mask)
-        x = self.head7(x, padding_mask)
-        x = self.head8(x, padding_mask)
+        # x = self.head4(x, padding_mask)
+        # x = self.head5(x, padding_mask)
+        # x = self.head6(x, padding_mask)
+        # x = self.head7(x, padding_mask)
+        # x = self.head8(x, padding_mask)
         x = self.project(x)
         return x
 
-    def train(self, x, y, optimizer, epochs):
+    def train(self, x, y, epochs, optimizer):
         for epoch in range(epochs):
             y_hat = self.forward(x)
+            # print(y_hat.shape, y.shape)
             loss = CrossEntropy(y_hat, y, axis=-1)
     
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            if epoch % 10 == 0:
+            if epoch % 1 == 0:
                 print(f"Epoch {epoch}, Loss: {loss.data}")
                 
 if __name__ == "__main__":
-    tok = Tokenizer(token_to_id_path="src/tokenizer/token_to_id.json", merges_path="src/tokenizer/merges.json")
-
-    D_MODEL = 2
-    VOCAB_SIZE = len(tok)
-    N_HEADS = 1
-    MAX_SEQ_LEN = 1024
+    D_MODEL = 48
+    VOCAB_SIZE = 20
+    N_HEADS = 2
+    MAX_SEQ_LEN = 32
     PAD_IDX = 0
-    model = Test(d_model=D_MODEL, n_heads=N_HEADS, vocab_size=VOCAB_SIZE, max_seq_len=MAX_SEQ_LEN, pad_idx=PAD_IDX)
-    text = "This asdf"
-    x = xp.array([tok.encode(text)])[:,:-1]
-    y = Tensor(xp.array([tok.encode(text)]), requires_grad=True)[:,1:]
 
-    optimizer = AdamW(model.parameters(), lr=0.01)
-
-    model.train(x, y, optimizer, epochs=5)
-
-    print(optimizer.params)
-
-    model.save_model(optimizer, "../../checkpoints/transformer_implementation")
-
-    print("-" * 100)
+    model = Net(d_model=D_MODEL, n_heads=N_HEADS, vocab_size=VOCAB_SIZE, max_seq_len=MAX_SEQ_LEN, pad_idx=PAD_IDX)
+    model._build((128, 15))
+    optimizer = AdamW(model.parameters(), lr=0.001, precision=(xp.float32, xp.float32))
 
 
-    new_model = Test(d_model=D_MODEL, n_heads=N_HEADS, vocab_size=VOCAB_SIZE, max_seq_len=MAX_SEQ_LEN, pad_idx=PAD_IDX)
-    new_model.load_model(optimizer, "../../checkpoints/transformer_implementation")
-    print(optimizer.params)
+    model.train(x_mine, y_mine, epochs=1000, optimizer=optimizer)
+
+
+    
         
