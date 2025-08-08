00d80cbe08fdce5d40907e8e99ef40143fa1fe24
Quinn Lue <luequinn@gmail.com>
2025-08-08 12:01:29 -0600

    fixed no gradsi in backprop

--- src/training/model.py
+++ src/training/model.py
@@ -114,7 +114,8 @@ class Model(Module):
             for i, batch in enumerate(tqdm(dl, desc=f"Training epoch {epoch}")):
                 y_hat = self.forward(batch[:,:-1])
                 loss = CrossEntropyWithLogits(y_hat, batch[:,1:])/self.mini_batch_per_step
-                loss_history.append(loss.data)
+                loss_history.append(float(loss.data[0]))
+
                 loss.backward()
                 self._gc()
                 if (i + 1) % self.mini_batch_per_step == 0:
