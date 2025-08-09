005a6884c6462542fe489d6397e2ca9806edc1c4
Quinn Lue <luequinn@gmail.com>
2025-08-09 15:10:42 -0600

    asdf

--- src/training/model.py
+++ src/training/model.py
@@ -113,7 +113,7 @@ class Model(Module):
         for epoch in range(self.epochs):
             for i, batch in enumerate(tqdm(dl, desc=f"Training epoch {epoch}")):
                 y_hat = self.forward(batch[:,:-1])
-                loss = CrossEntropyWithLogits(y_hat, batch[:,1:])/self.mini_batch_per_step
+                loss = CrossEntropyWithLogits(y_hat, batch[:,1:])
                 loss_history.append(float(loss.data))
                 loss.backward()
                 optimizer.step()
@@ -121,7 +121,7 @@ class Model(Module):
                 self._gc()
 
                 if (i + 1) % 1 == 0:
-                    train_logger.info(f"Training loss: {np.array(loss_history[-25:]).mean() * self.mini_batch_per_step}")
+                    train_logger.info(f"Training loss: {np.array(loss_history[-25:]).mean()}")
 
                 # checkpointing & validation
                 if time.perf_counter() - last_cp_time > self.CHECKPOINT_INTERVAL_SECONDS:
